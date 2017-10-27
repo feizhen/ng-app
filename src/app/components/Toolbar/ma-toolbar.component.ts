fcc599d1ab479be3c535cd89cebb03102ce72252
linzhen <linzhen@sensetime.com>
2017-10-27 11:35:32 +0800

    添加menu icon动画

--- src/app/components/Toolbar/ma-toolbar.component.ts
+++ src/app/components/Toolbar/ma-toolbar.component.ts
@@ -1,14 +1,33 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
 
+export const ICON_ANIMATION = "225ms";
 @Component({
   selector: "ma-toolbar",
   templateUrl: "./ma-toolbar.component.html",
-  styleUrls: ["./ma-toolbar.component.scss"]
+  styleUrls: ["./ma-toolbar.component.scss"],
+  animations: [
+    trigger("iconState", [
+      state("collapsed", style({ transform: "rotate(-90deg)" })),
+      state("expanded", style({ transform: "rotate(0)" })),
+      transition("expanded <=> collapsed", animate(ICON_ANIMATION))
+    ])
+  ]
 })
-export class MaToolbarComponent {
+export class MaToolbarComponent implements OnInit {
+  private isOpen: string;
   @Output() menuClick: EventEmitter<any> = new EventEmitter();
-
+  ngOnInit() {
+    this.isOpen = 'expanded';
+  }
   handleClick() {
+    this.isOpen = this.isOpen === 'expanded' ? 'collapsed' : 'expanded';
     this.menuClick.emit(null);
   }
 }
